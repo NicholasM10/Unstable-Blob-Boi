30aa53a89c6d38f96307dbc14eb30bd4b3531e2e
Nick M <ndplay@ymail.com>
2021-10-02 19:31:04 +0400

    Prefabs & Scripts

--- Unstable2DBoi/Assets/Character.cs
+++ Unstable2DBoi/Assets/Character.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    public float moveSpeed = 3f;
+    public float jumpSize = 3f;
+    public float unstabilityTime = 20f;
+    public float unstabilityVariation = 10f;
+
+    public Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        float translation = Input.GetAxis("Horizontal") * moveSpeed;
+        this.transform.Translate(translation, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpSize));
+        }
     }
 }
